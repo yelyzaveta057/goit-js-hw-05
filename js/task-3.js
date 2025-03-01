ed6d023dc84ca5d2eb5100a09fa3c4afa4abbf0e
yelyzaveta057 <lisa.rodionova2002@gmail.com>
2025-02-28 21:07:43 -0500

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,6 @@
 const sortByDescendingFriendCount = (users) => {
-    return users.sort((a, b) => b.friends.length - a.friends.length);
+  return users.toSorted((a, b) => b.friends.length - a.friends.length);
+
   };
 console.log(
     sortByDescendingFriendCount([
